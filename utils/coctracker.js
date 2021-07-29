134afa2591de7f76d3c994cd5049ee666c4cd565
Rohan <rohank2502@gmail.com>
2021-07-29 10:14:57 +0530

    chatroom app initial commit

---
+++ utils/coctracker.js
@@ -0,0 +1,11 @@
+var Filter = require('bad-words');
+var customFilter = new Filter({ placeHolder: '~' });
+function badwordstracker(message) {
+  const messagecheck = customFilter.clean(message);
+  if (messagecheck.includes('~')) {
+    return '~'
+  } else {
+    return messagecheck;
+  }
+}
+module.exports = badwordstracker;
